a9761161a63748bc50719e7ca329268cea552220
KyouKiSu <kyoukisu@gmail.com>
2022-07-23 03:19:14 +1100

    new email lib, code format, small fixes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import buildLogger from "./logger.js";
-import { default as ImapClient } from 'emailjs-imap-client'
+import imaps from 'imap-simple'
+import * as _ from 'underscore'
 import my_utils from "./utils/file_parse.js";
 
-const DELAY = 10000;
+const DELAY = 20000;
 const PASSWORD_EXTRA = "39tt!";
-const EXIT_AFTER_FINISHED = true;
+const KEEP_OPEN = false;
 
 const DIR_PATH = dirname(fileURLToPath(import.meta.url));
 let emails = my_utils.get_array_from_file(`${DIR_PATH}\\email.txt`).map(my_utils.format_email)
@@ -17,55 +18,82 @@ puppeteer.use(StealthPlugin());
 process.setMaxListeners(0);
 
 async function get_codes_droppp(user, pass, imap, port) {
-    let codes = [];
-    var client = new ImapClient.default(imap, port, {
-        logLevel: 40,
-        auth: {
+    let codes = []
+    var config = {
+        imap: {
             user: user,
-            pass: pass,
+            password: pass,
+            host: imap,
+            port: port,
+            tls: true,
+            authTimeout: 20000
         }
-    });
-    await client.connect()
-    const MAX_RETRIES = 10;
+    };
+    let connection = await imaps.connect(config)
+    await connection.openBox('INBOX')
+    var searchCriteria = ['1:*'];
+    var fetchOptions = {
+        bodies: ['HEADER', 'TEXT'],
+    };
+    const MAX_RETRIES = 5;
     let current_attempt = 0;
     const sleep_time = 2000;
     while (current_attempt < MAX_RETRIES) {
-        await client.listMessages('INBOX', '1:*', ['uid', 'flags', 'envelope', 'body[]'], { valueAsString: true }).then((messages) => {
-            messages.slice().reverse().forEach((message) => {
-                if (message.envelope.from[0].address == "support@droppp.io") {
-                    let reg = />\d\d\d\d\d\d</g
-                    let code = message["body[]"].match(reg)
-                    code = code[0].replace(">", "").replace("<", "")
-                    codes.push(code)
-                }
-            });
-        })
+        let messages = await connection.search(searchCriteria, fetchOptions)
+
+        messages.forEach(function (item) {
+            var headers = _.find(item.parts, { "which": "HEADER" })
+            let sender = String((headers['body']['from']))
+            if (sender.includes('support@droppp.io')) {
+                var text = _.find(item.parts, { "which": "TEXT" })['body']
+                let reg = />\d\d\d\d\d\d</g
+                let code = text.match(reg)
+                code = code[0].replace(">", "").replace("<", "")
+                codes.push(code)
+            }
+        });
         if (codes != []) break;
         current_attempt += 1;
         await new Promise(resolve => setTimeout(resolve, sleep_time))
     }
-    await client.close()
-    return codes;
+    connection.end()
+    return codes
 }
+
+async function wait_and_click(page, selector) {
+    await page.waitForSelector(selector, { timeout: 10000 });
+    await page.focus(selector)
+    await Promise.all([
+        await page.click(selector)
+    ]);
+
+    await new Promise(r => setTimeout(r, 400));
+
+}
+
+
+
 async function create_funko_account(email_obj, proxy_obj, i) {
     const loggers = buildLogger(DIR_PATH + "\\logs\\", email_obj[0]);
     let { logScreen } = loggers;
     let browser;
     let args = ['--disable-extensions-http-throttling', '--auto-open-devtools-for-tabs', `--proxy-server=${proxy_obj[2]}`,
-        `--disable-extensions-except=${DIR_PATH}\\anticaptcha`,
+        //`--disable-extensions-except=${DIR_PATH}\\anticaptcha`,
         `--load-extension=${DIR_PATH}\\anticaptcha`,]
 
     let page = '';
+    logScreen.info("Starting");
     try {
         browser = await puppeteer.launch({
-            userDataDir: `${DIR_PATH}\\profiles\\${i}\\`,
+            userDataDir: `${DIR_PATH}\\profiles\\${email_obj[0]}\\`,
             headless: false,
             ignoreDefaultArgs: ["--enable-automation"],
             args: args
         });
+        //logScreen.info("Starting")
         [page] = await browser.pages();
         await page.authenticate({ username: proxy_obj[0], password: proxy_obj[1] })
-        await page.goto('https://droppp.io/', { waitUntil: 'load' });
+        await page.goto('https://droppp.io/', { waitUntil: 'load', timeout: 120000 });
         await page.setDefaultTimeout(500000);
         await new Promise(r => setTimeout(r, 5000));
     }
@@ -75,83 +103,61 @@ async function create_funko_account(email_obj, proxy_obj, i) {
     }
 
     try {
-        logScreen.info("started creation")
-        try {
-            await Promise.all([
-                await page.click("#__next > div.styles_wrap__1GWKR > div.styles_container__HCN49 > div.styles_contentWrap__3vhmC > div.styles_intro__1eSrO > div.styles_links__3SQHD > button")
-            ]);
-            await new Promise(r => setTimeout(r, 2000));
-            await page.focus('#__next > div.styles_container__20C00 > div > div.styles_container__2vQ2_ > form > input:nth-child(1)')
-            await page.keyboard.type(email_obj[0])
-            await page.focus('#__next > div.styles_container__20C00 > div > div.styles_container__2vQ2_ > form > input:nth-child(2)')
-            await page.keyboard.type((email_obj[1] + PASSWORD_EXTRA))
-            await Promise.all([
-                await page.click("#__next > div.styles_container__20C00 > div > div.styles_container__2vQ2_ > form > button")
-            ]);
-        }
-        catch (e) {
-            //logScreen.error(e)
-            //prly awaiting for email
-            await Promise.all([
-                await page.click("#__next > div.styles_header__I2p04 > div > a")
-            ]);
 
-        }
+        logScreen.info("Started")
 
-        try {
-            //await page.waitForNavigation({ waitUntil: 'networkidle2' });
-            await new Promise(r => setTimeout(r, 1500));
-            if ((await page.content()).match('An account is already associated with this email.')) {
-                throw 'rip'
+        if ((await page.content()).match(`>Profile<`)) {
+            logScreen.info('Has auth');
+
+            await wait_and_click(page, "#__next > div.styles_header__I2p04 > div > a")
+        } else {
+            try {
+                await wait_and_click(page, "#__next > div.styles_wrap__1GWKR > div.styles_container__HCN49 > div.styles_contentWrap__3vhmC > div.styles_intro__1eSrO > div.styles_links__3SQHD > button")
+                await wait_and_click(page, "#__next > div.styles_container__20C00 > div > div.styles_container__F3xbE > form > input")
+                await page.keyboard.type(email_obj[0])
+                await wait_and_click(page, "#__next > div.styles_container__20C00 > div > div.styles_container__F3xbE > form > button")
+                await wait_and_click(page, '#__next > div.styles_container__20C00 > div > div.styles_container__F3xbE > form > div > div > input')
+                await page.keyboard.type((email_obj[1] + PASSWORD_EXTRA))
+                await wait_and_click(page, "#__next > div.styles_container__20C00 > div > div.styles_container__F3xbE > form > button")
             }
-
-
-            await page.waitForSelector("#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > div.styles_container__3tX-w > div > input[type=tel]:nth-child(1)")
-        }
-        catch (e) {
-            logScreen.info("already registered")
-            await page.goto('https://droppp.io/login/', { waitUntil: 'load' })
-            await page.focus('#__next > div.styles_container__20C00 > div > div.styles_container__37UFk > form > input:nth-child(1)')
-            await page.keyboard.type(email_obj[0])
-            await page.focus('#__next > div.styles_container__20C00 > div > div.styles_container__37UFk > form > input:nth-child(2)')
-            await page.keyboard.type((email_obj[1] + PASSWORD_EXTRA))
-            await Promise.all([
-                await page.click("#__next > div.styles_container__20C00 > div > div.styles_container__37UFk > form > button")
-            ]);
-            await new Promise(r => setTimeout(r, 1500));
-            if (EXIT_AFTER_FINISHED) {
-                browser.close()
+            catch (e) {
+                logScreen.error(page, "Already in")
             }
+        }
+        try {
+            await page.waitForSelector("#__next > div.styles_container__1cJGR > div.styles_logoContainer__2lTns > div.styles_container__2gBt_.styles_logo__2ZC3F > div > svg", { timeout: 8000 })
+            logScreen.info("Logged in")
+            if (!KEEP_OPEN) browser.close()
+            return;
+        } catch (e) {
+            logScreen.info("Have to complete email check")
         }
         let passed = false;
         for (let retry = 0; retry < 5 && !passed; retry++) {
-            let resend_exists = false;
-            try {
-                await page.waitForSelector("#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > div.styles_links__31NJI > div", { timeout: 1000 });
-                if (retry > 0) {
-                    await Promise.all([
-                        await page.click("#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > div.styles_links__31NJI > div")
-                    ]);
-                }
-                resend_exists = true;
-            } catch (error) {
-                passed = true;
+            logScreen.info("Email code, attempt # " + retry)
+            await page.focus('#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > div.styles_container__3tX-w > div > input[type=tel]:nth-child(1)')
+            logScreen.info("Waiting for code")
+            await new Promise(r => setTimeout(r, 5000));
+            let codes = await get_codes_droppp(email_obj[0], email_obj[1], email_obj[2], email_obj[3])
+            logScreen.info("Codes:    " + codes)
+            let code = codes[codes.length - 1]
+            logScreen.info("Received: " + code)
+            await page.keyboard.type(code)
+            await new Promise(r => setTimeout(r, 500));
+            // next button
+            await wait_and_click(page, "#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > button")
+            // invalid
+            await new Promise(r => setTimeout(r, 2000));
+            if ((await page.content()).match(`Invalid code. Please try again.`)) {
+                continue;
             }
-            if (resend_exists) {
-                await page.focus('#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > div.styles_container__3tX-w > div > input[type=tel]:nth-child(1)')
-                logScreen.info("waiting for code")
-                await new Promise(r => setTimeout(r, 5000));
-                let code = await get_codes_droppp(email_obj[0], email_obj[1], email_obj[2], email_obj[3])
-                code = code[0]
-                logScreen.info("received " + code)
-                await page.keyboard.type(code)
-                await new Promise(r => setTimeout(r, 500));
-                await page.waitForSelector("#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > button")
-                await Promise.all([
-                    await page.click("#__next > div.styles_container__20C00 > div > div.styles_container__1DJtJ > form > button")
-                ]);
-                await new Promise(r => setTimeout(r, 500));
-            }
+            passed = true;
+            break;
+        }
+        if (passed == false) {
+            logScreen.info("Can't pass email check")
+            if (!KEEP_OPEN) browser.close()
+            return;
         }
 
         //mailing list
@@ -175,19 +181,18 @@ async function create_funko_account(email_obj, proxy_obj, i) {
             await new Promise(r => setTimeout(r, 2000));
         }
         catch (e) {
-            let sr = 1;
+            let _ = 1;
         }
-        logScreen.info('created')
+        logScreen.info('Created')
     }
     catch (e) {
         logScreen.error(e)
     }
-    if (EXIT_AFTER_FINISHED) {
-        browser.close()
-    }
+    if (!KEEP_OPEN) browser.close()
+    return;
 }
 const O = 0
-const N = emails.length;
+const N = 1
 for (let i = O; i < N; i++) {
     try {
         create_funko_account(emails[i], proxies[i % proxies.length], i)
--- src/utils/file_parse.js
+++ src/utils/file_parse.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 let my_utils = {
-    get_array_from_file: function (filename){
+    get_array_from_file: function (filename) {
         let data = fs.readFileSync(filename);
         let array = data.toString().split("\n");
         array = array.filter(function (el) {
-            return el != null && el != '';
+            return el != null && el != '' && el != ' ';
         });
         for (let index = 0; index < array.length; index++) {
             const element = array[index].trim();
@@ -12,17 +12,17 @@ let my_utils = {
         }
         return array;
     },
-    format_proxy: function (line){
+    format_proxy: function (line) {
         let array = [];
-        let splited=line.split("@");
-        let splited2=splited[0].split(":");
-        array=array.concat(splited2);
+        let splited = line.split("@");
+        let splited2 = splited[0].split(":");
+        array = array.concat(splited2);
         array.push(splited[1]);
         return array
     },
-    format_email: function (line){
+    format_email: function (line) {
         let a = line.split(":");
-        a[3]=parseInt(a[3]);
+        a[3] = parseInt(a[3]);
         return a
     },
 };
